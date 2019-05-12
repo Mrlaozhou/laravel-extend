90c6c86638747caab2a827be627eef7bbab7ab50
Luke <zgsself@163.com>
2019-05-12 16:05:51 +0800

    add.unlimitedable

---
+++ src/Unlimitedable.php
@@ -0,0 +1,127 @@
+<?php
+namespace Mrlaozhou\Extend;
+
+use Illuminate\Support\Facades\Cache;
+
+/**
+ * Trait UnlimitedCacheService
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder query()
+ */
+trait Unlimitedable
+{
+    /**
+     * 获取所有数据
+     *
+     * @return \Mrlaozhou\Extend\Collection
+     */
+    public static function unlimitedCollection()
+    {
+        return static::unlimitedDataCollection();
+    }
+
+    /**
+     * 根据ID获取子集
+     *
+     * @param int $id
+     *
+     * @return \Mrlaozhou\Extend\Collection
+     */
+    public static function unlimitedCollectionById(int $id)
+    {
+        if( $parent = static::unlimitedSingleById($id) ) {
+            return static::unlimitedCollection()->toLists($parent->getKey(), null, 1, 'id', 'parent_id');
+        }
+        return new Collection();
+    }
+
+    /**
+     * 根据keyword获取子集
+     *
+     * @param string $keyword
+     *
+     * @return \Mrlaozhou\Extend\Collection
+     */
+    public static function unlimitedCollectionByKeyword(string $keyword)
+    {
+        if( $parent = static::unlimitedSingleByKeyword($keyword) ) {
+            return static::unlimitedCollection()->toLists($parent->getKey(), null, 1, 'id', 'parent_id');
+        }
+        return new Collection();
+    }
+
+    /**
+     * 通过ID获取单一数据
+     * @param int $id
+     *
+     * @return static|\Illuminate\Database\Eloquent\Model
+     */
+    public static function unlimitedSingleById(int $id)
+    {
+        return static::unlimitedCollection()->where('id', $id)->first();
+    }
+
+    /**
+     * 通过keyword获取单一数据
+     * @param string $keyword
+     *
+     * @return static
+     */
+    public static function unlimitedSingleByKeyword(string $keyword)
+    {
+        return static::unlimitedCollection()->where('keyword', $keyword)->first();
+    }
+
+    /**
+     * @return int|null
+     */
+    public function unlimitedId()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * @return int|null
+     */
+    public function unlimitedPid()
+    {
+        return $this->parent_id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function unlimitedName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function unlimitedKeyword()
+    {
+        return $this->keyword;
+    }
+
+    /**
+     * 原始数据集
+     *
+     * @return \Mrlaozhou\Extend\Collection
+     */
+    protected static function unlimitedDataCollection()
+    {
+        return new Collection(
+            Cache::remember( static::unlimitedCacheKey(), 3600, function () {
+                return static::query()->select('id', 'name', 'keyword', 'parent_id')->get();
+            } )
+        );
+    }
+
+    /**
+     * 获取缓存标识
+     *
+     * @return string
+     */
+    abstract protected static function unlimitedCacheKey();
+}
